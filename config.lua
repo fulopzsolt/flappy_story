7846342417fbf5572f66a84b53e4c252d46dc783
fulopzsolt <fulopzsolt@reea.net>
2014-05-27 01:03:19 +0300

    first commit

---
+++ config.lua
@@ -0,0 +1,26 @@
+--calculate the aspect ratio of the device:
+local aspectRatio = display.pixelHeight / display.pixelWidth
+
+
+
+application = {
+   settings =
+{
+    orientation =
+    {
+        default = "landscapeRight",
+        supported = { "landscapeLeft", "landscapeRight" },
+    },
+},
+   content = {
+	  -- graphicsCompatibility = 1,  -- Turn on V1 Compatibility Mode
+      width = aspectRatio > 1.5 and 800 or math.ceil( 1200 / aspectRatio ),
+      height = aspectRatio < 1.5 and 1200 or math.ceil( 800 * aspectRatio ),
+      scale = "letterBox",
+      fps = 30,
+
+      imageSuffix = {
+         ["@2x"] = 1.3
+      },
+   },
+}
---
+++ game.lua
@@ -0,0 +1,284 @@
+local physics = require "physics"
+physics.start()
+physics.setGravity( 0, 0 )
+
+local mydata = require( "mydata" )
+local storyboard = require ("storyboard")
+local scene = storyboard.newScene()
+
+mydata.score = 0
+
+function scene:createScene(event)
+	physics.setDrawMode("hybrid")
+	local screenGroup = self.view
+
+	local background = display.newImage("bg.png")
+	screenGroup:insert(background)
+
+    bg = display.newImageRect('bg.png',900,1425)
+	bg.anchorX = 0
+	bg.anchorY = 1
+	bg.x = 0
+	bg.y = display.contentHeight
+	bg.speed = 4
+	screenGroup:insert(bg)
+    
+    elements = display.newGroup()
+	elements.anchorChildren = true
+	elements.anchorX = 0
+	elements.anchorY = 1
+	elements.x = 0
+	elements.y = 0
+	screenGroup:insert(elements)
+
+	ground = display.newImageRect('ground.png',900,162)
+	ground.anchorX = 0
+	ground.anchorY = 1
+	ground.x = 0
+	ground.y = display.contentHeight
+	screenGroup:insert(ground)
+
+	platform = display.newImageRect('platform.png',900,53)
+	platform.anchorX = 0
+	platform.anchorY = 1
+	platform.x = 0
+	platform.y = display.viewableContentHeight - 110
+	physics.addBody(platform, "static", {density=.1, bounce=0.1, friction=.2})
+	platform.speed = 4
+	screenGroup:insert(platform)
+
+	platform2 = display.newImageRect('platform.png',900,53)
+	platform2.anchorX = 0
+	platform2.anchorY = 1
+	platform2.x = platform2.width
+	platform2.y = display.viewableContentHeight - 110
+	physics.addBody(platform2, "static", {density=.1, bounce=0.1, friction=.2})
+	platform2.speed = 4
+	screenGroup:insert(platform2)
+	
+	p_options = 
+	{
+		-- Required params
+		width = 80,
+		height = 42,
+		numFrames = 2,
+		-- content scaling
+		sheetContentWidth = 160,
+		sheetContentHeight = 42,
+	}
+
+	playerSheet = graphics.newImageSheet( "bat.png", p_options )
+	player = display.newSprite( playerSheet, { name="player", start=1, count=2, time=500 } )
+	player.anchorX = 0.5
+	player.anchorY = 0.5
+	player.x = display.contentCenterX - 150
+	player.y = display.contentCenterY
+	physics.addBody(player, "static", {density=10, bounce=0.1, friction=3})
+	-- local physicsData = (require "bat").physicsData(scaleFactor)
+	-- physics.addBody( player, "static", physicsData:get("bat") )
+	player:applyForce(0, -300, player.x, player.y)
+	player:play()
+	screenGroup:insert(player)
+	
+	scoreText = display.newText(mydata.score,display.contentCenterX,
+	150, "pixelmix", 58)
+	scoreText:setFillColor(0,0,0)
+	scoreText.alpha = 0
+	screenGroup:insert(scoreText)
+	
+	instructions = display.newImageRect("instructions.png",400,328)
+	instructions.anchorX = 0.5
+	instructions.anchorY = 0.5
+	instructions.x = display.contentCenterX
+	instructions.y = display.contentCenterY
+	screenGroup:insert(instructions)
+	
+end
+
+
+function onCollision( event )
+	if ( event.phase == "began" ) then
+		storyboard.gotoScene( "restart" )	
+	end
+end
+
+function platformScroller(self,event)
+	
+	if self.x < (-900 + (self.speed*2)) then
+		self.x = 900
+	else 
+		self.x = self.x - self.speed
+	end
+	
+end
+
+
+function getNextHeight()
+	local nextY = math.random(player.y - 50, player.y + 50)
+	if (nextY > 10 and nextY < display.contentHeight - 10) then
+	 	return nextY
+	else
+		nextY = getNextHeight()
+	end
+end
+local gameStarted = false
+local nextHeight = getNextHeight()
+function flyUp(event)
+   if event.phase == "began" then
+		if gameStarted == false then
+			player.bodyType = "dynamic"
+			instructions.alpha = 0
+			scoreText.alpha = 1
+			addColumns()
+			moveColumnTimer = timer.performWithDelay(2, moveColumns, -1)
+		 	flyTheBirdTimer = timer.performWithDelay(100, flyTheBird, -1)
+			gameStarted = true
+			player:applyForce(0, -300, player.x, player.y)
+		end
+	end
+end
+function flyTheBird()
+		if (player.y < nextHeight - 20) then 
+			player:applyForce(0, 200, player.x, player.y)
+		elseif (player.y > nextHeight + 20 ) then 
+			player:applyForce(0, -200, player.x, player.y)
+		else 
+			nextHeight = getNextHeight()
+		end
+		local vx, vy = player:getLinearVelocity()
+		print(player.y..'__nnn')
+		
+		print(player.y)
+end
+
+
+function moveColumns()
+		for a = elements.numChildren,1,-1  do
+			elements[a].tag ='column'
+			-- elements[a]:addEventListener("touch", addDragMove)
+			if(elements[a].x < display.contentCenterX - 170) then
+				if elements[a].scoreAdded == false then
+					
+					mydata.score = mydata.score + 1
+					scoreText.text = mydata.score
+					elements[a].scoreAdded = true
+				end
+			end
+			if(elements[a].x < display.contentCenterX - 170) then
+				if (elements[a].added == false) then
+					elements[a].added = true
+				addColumns()
+				end
+				
+			end
+			if(elements[a].x > -300) then
+				elements[a].x = elements[a].x - 5
+			else 
+				elements[a]:removeEventListener("touch", addDragMove)
+				
+				elements[a]:removeSelf()
+				elements[a] = nil
+				-- elements:remove(elements[a])
+				
+			end	
+		end
+		
+		
+end
+
+function addDragMove(event)
+		print(event.target.tag)
+		goodTouch = false
+	    if event.phase == "began" then
+
+			if (event.target.tag == "column") then
+				goodTouch = true
+				objectToMove = event.target
+			end
+	        markX = event.x    -- store x location of object
+	        markY = event.y    -- store y location of object
+			return false
+	    elseif event.phase == "moved" then
+
+			
+				local y = (event.y - event.yStart) + markY
+		        if (event.y - markY > 10 or event.y - markY < -10) then
+		        	objectToMove.y = y    -- move object based on calculations above
+		        end
+				        
+	    end
+	    
+	    return true
+	
+
+end
+
+function addColumns()
+	
+
+	height = 500
+	local physicsData = (require "column").physicsData(scaleFactor)
+	column = display.newImageRect('column.png',100,1914)
+	column.anchorX = 0.5
+	column.anchorY = 0.5
+	column.x = display.contentWidth + 100
+	column.y = height - 160
+	column.tag = 'column'
+	column.added = false
+	column.scoreAdded = false
+	-- physics.addBody(column, "kinematic", {density=1, bounce=0.1, friction=.2})
+	
+	physics.addBody( column, "kinematic", physicsData:get("column3") )
+	column:addEventListener("touch", addDragMove)
+	elements:insert(column)
+
+end	
+
+local function checkMemory()
+   collectgarbage( "collect" )
+   local memUsage_str = string.format( "MEMORY = %.3f KB", collectgarbage( "count" ) )
+   print( memUsage_str, "TEXTURE = "..(system.getInfo("textureMemoryUsed") / (1024 * 1024) ) )
+end
+
+
+function scene:enterScene(event)
+	
+	storyboard.removeScene("start")
+	Runtime:addEventListener("touch", flyUp)
+
+	platform.enterFrame = platformScroller
+	Runtime:addEventListener("enterFrame", platform)
+
+	platform2.enterFrame = platformScroller
+	Runtime:addEventListener("enterFrame", platform2)
+    
+    Runtime:addEventListener("collision", onCollision)
+	memTimer = timer.performWithDelay( 1000, checkMemory, 0 )
+
+end
+function payerPos()
+	print(player.y..'____yyyyy')
+end
+function scene:exitScene(event)
+
+	Runtime:removeEventListener("touch", flyUp)
+	Runtime:removeEventListener("enterFrame", platform)
+	Runtime:removeEventListener("enterFrame", platform2)
+	Runtime:removeEventListener("collision", onCollision)
+	timer.cancel(flyTheBirdTimer)
+	timer.cancel(moveColumnTimer)
+	timer.cancel(memTimer)
+	
+end
+
+function scene:destroyScene(checkMemory)
+
+end
+
+
+scene:addEventListener("createScene", scene)
+scene:addEventListener("enterScene", scene)
+scene:addEventListener("exitScene", scene)
+scene:addEventListener("destroyScene", scene)
+
+return scene
